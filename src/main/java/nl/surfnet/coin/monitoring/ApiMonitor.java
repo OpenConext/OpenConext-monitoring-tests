9b2fabc074859d0192852138fe152e43a686ceb9
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2013-11-20 13:00:44 +0100

    added gitignore. Made ApiMonitor use correct return codes

--- src/main/java/nl/surfnet/coin/monitoring/ApiMonitor.java
+++ src/main/java/nl/surfnet/coin/monitoring/ApiMonitor.java
@@ -27,46 +27,51 @@ public class ApiMonitor {
 
   public static void main(String[] args) throws Exception {
 
-    LOG.debug("Running API monitor. Input parameters: {}", Arrays.asList(args));
-
-    assertEquals("Should have 4 parameters: API base URL, client ID, secret, personId to test", 4, args.length);
-    String apiBaseUrl = args[0];
-    String clientId = args[1];
-    String secret = args[2];
-    String personId = args[3];
-
-
-    OpenConextOAuthClientImpl client = new OpenConextOAuthClientImpl();
-
-    client.setConsumerKey(clientId);
-    client.setConsumerSecret(secret);
-    client.setEndpointBaseUrl(apiBaseUrl);
-
-    // OnBehalfOf is null: this yields a client-credentials-call to API.
-
-    // Existing user.
-    Person person = client.getPerson(personId, null);
-    assertNotNull(person);
-    assertEquals("Geert van der Ploeg", person.getDisplayName());
-
-    // Non-existent user
     try {
-      client.getPerson(nonExistingPersonId, null);
-      fail("Person-call with non-existent user should throw exception");
-    } catch (RuntimeException e) {
-      assertTrue("Response of person-call with non-existent user should be a 404", e.getMessage().contains("Error response: 404"));
-    }
+      LOG.debug("Running API monitor. Input parameters: {}", Arrays.asList(args));
 
-    // Groups of existing user
-    List<Group> groups = client.getGroups(personId, null);
-    assertNotNull("Groups-call for person " + personId + " should not be null", groups);
+      assertEquals("Should have 4 parameters: API base URL, client ID, secret, personId to test", 4, args.length);
+      String apiBaseUrl = args[0];
+      String clientId = args[1];
+      String secret = args[2];
+      String personId = args[3];
 
-    // Groups of non-existing user
-    try {
-      client.getGroups(nonExistingPersonId, null);
-      fail("Groups-call with non-existent user should throw exception");
-    } catch (RuntimeException e) {
-      assertTrue("Response of groups-call with non-existent user should be a 404", e.getMessage().contains("Error response: 404"));
+
+      OpenConextOAuthClientImpl client = new OpenConextOAuthClientImpl();
+
+      client.setConsumerKey(clientId);
+      client.setConsumerSecret(secret);
+      client.setEndpointBaseUrl(apiBaseUrl);
+
+      // OnBehalfOf is null: this yields a client-credentials-call to API.
+
+      // Existing user.
+      Person person = client.getPerson(personId, null);
+      assertNotNull(person);
+      assertEquals("Geert van der Ploeg", person.getDisplayName());
+
+      // Non-existent user
+      try {
+        client.getPerson(nonExistingPersonId, null);
+        fail("Person-call with non-existent user should throw exception");
+      } catch (RuntimeException e) {
+        assertTrue("Response of person-call with non-existent user should be a 404", e.getMessage().contains("Error response: 404"));
+      }
+
+      // Groups of existing user
+      List<Group> groups = client.getGroups(personId, null);
+      assertNotNull("Groups-call for person " + personId + " should not be null", groups);
+
+      // Groups of non-existing user
+      try {
+        client.getGroups(nonExistingPersonId, null);
+        fail("Groups-call with non-existent user should throw exception");
+      } catch (RuntimeException e) {
+        assertTrue("Response of groups-call with non-existent user should be a 404", e.getMessage().contains("Error response: 404"));
+      }
+    } catch (Throwable e) {
+      LOG.error("Exception while running, will exit(2)", e);
+      System.exit(2);
     }
   }
 }
