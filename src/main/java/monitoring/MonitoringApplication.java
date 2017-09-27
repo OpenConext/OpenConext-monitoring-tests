100431df506787c54994fdd72cc4fa456fff3d72
oharsta <oharsta@zilverline.com>
2017-09-27 11:15:51 +0200

    Ensure all health information is shown

--- src/main/java/monitoring/MonitoringApplication.java
+++ src/main/java/monitoring/MonitoringApplication.java
@@ -3,7 +3,13 @@ package monitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.TraceWebFilterAutoConfiguration;
+import org.springframework.boot.actuate.endpoint.HealthEndpoint;
+import org.springframework.boot.actuate.endpoint.mvc.HealthMvcEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @SpringBootApplication(exclude = {
     TraceWebFilterAutoConfiguration.class,
@@ -15,4 +21,16 @@ public class MonitoringApplication {
         SpringApplication.run(MonitoringApplication.class, args);
     }
 
+
+    @Bean
+    public HealthMvcEndpoint exposeDetailsHealthMvcEndpoint(HealthEndpoint delegate) {
+        return new HealthMvcEndpoint(delegate) {
+
+            @Override
+            protected boolean exposeHealthDetails(HttpServletRequest request, Principal principal) {
+                return true;
+            }
+
+        };
+    }
 }
